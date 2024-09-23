8e9bb0fb9b450108670986f03ced0884203f356f
Lulox <lulox.eth@protonmail.com>
2024-09-23 05:59:26 -0300

    Small fix to textarea border

--- packages/nextjs/app/create/_components/DescriptionInput.tsx
+++ packages/nextjs/app/create/_components/DescriptionInput.tsx
@@ -46,10 +46,10 @@ export const DescriptionInput = <T extends { toString: () => string } | undefine
   }, [reFocus]);
 
   return (
-    <div className={`flex border-2 border-base-300 bg-base-200 rounded-lg text-accent ${modifier}`}>
+    <div className={`flex bg-base-200 rounded-lg text-accent ${modifier}`}>
       {prefix}
       <textarea
-        className="textarea textarea-ghost focus:border-green-600 focus:border-2 rounded-lg focus:outline-none focus:bg-transparent focus:text-gray-400 h-auto min-h-[3rem] px-4 border w-full font-medium placeholder:text-accent/50 text-green-500 resize-none"
+        className="textarea textarea-ghost border-base-300 focus:border-green-600 focus:border-2 rounded-lg focus:outline-none focus:bg-transparent focus:text-gray-400 h-auto min-h-[3rem] px-4 border w-full font-medium placeholder:text-accent/50 text-green-500 resize-none"
         placeholder={placeholder}
         name={name}
         value={value?.toString()}
