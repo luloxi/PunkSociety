dd1268d26ced54b05623315b2c16d2cb7ccc6638
Lulox <lulox.eth@protonmail.com>
2024-09-26 11:48:38 -0300

    Centered error on not-found lol

--- packages/nextjs/app/not-found/NotFound.tsx
+++ packages/nextjs/app/not-found/NotFound.tsx
@@ -4,7 +4,7 @@ import { NextPage } from "next";
 
 export const NotFound: NextPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center p-2">
+    <div className="flex flex-col justify-center items-center min-h-screen p-2">
       {/* Not Found Section */}
       <div className="relative text-red-500 flex flex-col justify-center items-center bg-base-100 p-6 rounded-lg shadow-md w-full">
         <p className="font-bold text-2xl">Page under development</p>
