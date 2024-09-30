ea4bc12e109808db91ac3213a210c7e82f26df5a
Lulox <lulox.eth@protonmail.com>
2024-09-30 13:42:03 -0300

    Changed a little margin

--- packages/nextjs/components/punk-society/NewsFeed.tsx
+++ packages/nextjs/components/punk-society/NewsFeed.tsx
@@ -9,7 +9,7 @@ type NewsFeedProps = {
 export const NewsFeed: React.FC<NewsFeedProps> = ({ posts }) => {
   return (
     <div className="mt-4 lg:mt-0 lg:flex lg:justify-center lg:items-center">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-3 lg:mx-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-3 lg:m-2">
         {posts.map(post => (
           <PostCard post={post} key={post.uri} />
         ))}
