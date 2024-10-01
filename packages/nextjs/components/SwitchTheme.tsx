7f295ffcdfd4d2c6adb874c467d1fb29c420335f
Luciano Oliva Bianco <lulox.eth@protonmail.com>
2024-10-01 19:40:31 -0300

    Update SwitchTheme.tsx

--- packages/nextjs/components/SwitchTheme.tsx
+++ packages/nextjs/components/SwitchTheme.tsx
@@ -26,13 +26,6 @@ export const SwitchTheme = ({ className }: { className?: string }) => {
 
   return (
     <div className={`flex space-x-2 h-8 items-center justify-center text-sm ${className}`}>
-      <input
-        id="theme-toggle"
-        type="checkbox"
-        className="toggle toggle-primary bg-primary hover:bg-primary border-primary"
-        onChange={handleToggle}
-        checked={isDarkMode}
-      />
       <label htmlFor="theme-toggle" className={`swap swap-rotate ${!isDarkMode ? "swap-active" : ""}`}>
         <SunIcon className="swap-on h-5 w-5" />
         <MoonIcon className="swap-off h-5 w-5" />
