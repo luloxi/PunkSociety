9fc3c0b7477df79dce4bb196b3593d0e6afe5bd6
Lulox <lulox.eth@protonmail.com>
2024-10-28 00:47:11 -0300

    Updated scaffold.config.js to match the README

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -8,7 +8,7 @@ const rpcUrl = process.env.NEXT_PUBLIC_PUNKSOCIETY_RPC_URL || "http://127.0.0.1:
 const wsRpcUrl = rpcUrl.replace(/^https:\/\//, "ws://");
 
 export const punksociety = /*#__PURE__*/ defineChain({
-  id: 7_615_243,
+  id: 615_243,
   name: "PunkSociety",
   nativeCurrency: {
     decimals: 18,
