99636158a2c4c822fdfa1d8a19ace1fa6e69cab1
Lulox <lulox.eth@protonmail.com>
2024-11-19 13:42:24 -0300

    Improvements to about page

--- packages/nextjs/app/about/About.tsx
+++ packages/nextjs/app/about/About.tsx
@@ -24,19 +24,19 @@ export const About = () => {
             height={150}
           />
           <div>
-            <h1 className="text-6xl font-mono text-center">
+            <h1 className="text-4xl font-bold font-mono text-center">
               What if you <strong className="text-green-600">earned</strong> 💸 from using social media?
             </h1>
             <p className="pt-6">
-              <div className="flex justify-center items-center gap-1 flex-wrap">
-                <span className="text-3xl text-center">
+              <div className="flex justify-center items-center gap-1 flex-wrap text-center">
+                <span className="text-2xl">
                   What if <span className="text-orange-600 font-bold">every like ❤️, comment 💬, and share 🔄</span>{" "}
                   <span className="text-green-600">changed the</span>
                 </span>
-                <span className="text-3xl px-1 text-blue-600 font-bold">USDC</span>
-                <Image src="/usdc-logo.png" alt="USDC" width={30} height={30} />
-                <span className="text-3xl">balance of your life? 🤔</span>
-                <span className="text-3xl">
+                <span className="text-2xl px-1 text-blue-600 font-bold">USDC</span>
+                <Image src="/usdc-logo.png" alt="USDC" width={30} height={30} className="inline-block" />
+                <span className="text-2xl">balance of your life? 🤔</span>
+                <span className="text-2xl">
                   Meet <strong>PunkSociety 🤘</strong>!
                 </span>
               </div>
@@ -45,144 +45,144 @@ export const About = () => {
           </div>
         </div>
       </div>
-      <div className="w-full pt-2 bg-base-300">
+      <div className="w-full pt-6 bg-base-300">
         <h1 className="text-6xl font-mono text-center">Features! 🤘</h1>
       </div>
       <div className="hero bg-base-300 flex flex-wrap justify-around gap-3 lg:p-4">
-        <div className="card lg:h-[500px] max-w-[600px] flex flex-col justify-between  lg:rounded-xl bg-base-100 shadow-xl">
-          <figure className=" h-[250px] mt-10 flex-shrink-0">
+        <div className="card lg:h-[500px] max-w-[400px] flex flex-col justify-between  lg:rounded-xl bg-base-100 shadow-xl">
+          <figure className=" h-[250px]  flex-shrink-0">
             <Image src="/socialmonetization.jpg" alt="Social monetization" className="" width={500} height={500} />
           </figure>
           <div className="card-body items-center text-center flex-grow overflow-hidden">
-            <h2 className="card-title text-3xl font-mono">Social monetization!</h2>
+            <h2 className="card-title text-2xl font-mono">Social monetization! ❤️</h2>
             <p>
               When liking, commenting and sharing, you send USDC to the post creator. If you receive a lot of
               interactions, you&apos;ll receive a lot of USDC!
             </p>
           </div>
         </div>
-        <div className="card lg:h-[500px] max-w-[600px] flex flex-col justify-between items-center bg-base-100 shadow-xl">
-          <div className="lg:h-[300px] mt-10 w-full overflow-hidden flex justify-center items-center">
-            <figure className="px-10 pt-10 flex-shrink-0">
+        <div className="card lg:h-[500px] max-w-[400px] flex flex-col justify-between items-center bg-base-100 shadow-xl">
+          <div className="lg:h-[300px] w-full overflow-hidden flex justify-center items-center">
+            <figure className="px-10 flex-shrink-0">
               <Image src="/usdc-yield.jpg" alt="Revenue" className=" object-cover" width={500} height={500} />
             </figure>
           </div>
           <div className="card-body items-center text-center flex-grow overflow-hidden">
-            <h2 className="card-title text-3xl font-mono">Earn USDC yield!</h2>
-            <p>When bridged to our Avalanche L1, USDC gets invested and generates interest on the home blockchain. </p>
+            <h2 className="card-title text-2xl font-mono">Earn USDC yield! 🫰</h2>
+            <p>When bridged to our Avalanche L1, USDC generates interest on the home blockchain. </p>
           </div>
         </div>
 
-        <div className="card lg:h-[500px] max-w-[600px] flex flex-col justify-between bg-base-100 shadow-xl">
+        <div className="card lg:h-[500px] max-w-[400px] flex flex-col justify-between bg-base-100 shadow-xl">
           <figure className="px-10 pt-10 flex-shrink-0">
             <Image src="/usdc-flag.jpg" alt="USDC Flag" className="" width={500} height={500} />
           </figure>
           <div className="card-body items-center text-center flex-grow overflow-hidden">
-            <h2 className="card-title text-3xl font-mono">USDC is the native gas here!</h2>
-            <p>1 USD = 1 USDC = native gas! Nothing else is needed to pay for transactions and interactions.</p>
+            <h2 className="card-title text-2xl font-mono">USDC = native gas! ⛽</h2>
+            <p>Only USDC is needed to pay for transactions and interactions.</p>
           </div>
         </div>
 
-        <div className="card lg:h-[500px] max-w-[600px] flex flex-col justify-between items-center bg-base-100 shadow-xl">
-          <div className="lg:h-[300px] mt-10 w-full overflow-hidden flex justify-center items-center">
-            <figure className="px-10 pt-10 flex-shrink-0">
-              <Image src="/revenue.jpg" alt="Revenue" className="object-cover" width={500} height={500} />
-            </figure>
-          </div>
-          <div className="card-body items-center text-center flex-grow overflow-hidden">
-            <h2 className="card-title text-3xl font-mono">Track your revenue!</h2>
-            <p>Analyze your earnings and spendings in your revenue dashboard.</p>
-          </div>
-        </div>
-
-        <div className="card lg:h-[500px] max-w-[600px] flex flex-col justify-between bg-base-100 shadow-xl">
-          <figure className="px-10 pt-10 flex-shrink-0">
-            <Image src="/social-login.jpeg" alt="Notifications" className="" width={500} height={500} />
-          </figure>
-          <div className="card-body items-center text-center flex-grow overflow-hidden">
-            <h2 className="card-title text-3xl font-mono">Social login!</h2>
-            <p>Access with your wallet or your social accounts!</p>
-          </div>
-        </div>
-        <div className="card lg:h-[500px] max-w-[600px] flex flex-col justify-between bg-base-100 shadow-xl">
+        <div className="card lg:h-[500px] max-w-[400px] flex flex-col justify-between bg-base-100 shadow-xl">
           <figure className="px-10 pt-10 flex-shrink-0">
             <Image src="/post-types.png" alt="Post types" className="" width={500} height={500} />
           </figure>
           <div className="card-body items-center text-center flex-grow overflow-hidden">
-            <h2 className="card-title text-3xl font-mono">Several post types!</h2>
-            <p>
-              Posts can have text, image, audio, video, links and polls.
-              <br /> And yeah, they&apos;re all NFTs! 🤯
-            </p>
+            <h2 className="card-title text-2xl font-mono">Post whatever! 🤹‍♂️</h2>
+            <p>Posts can have text, image, audio, video, links and polls. And yeah, they&apos;re all NFTs! 🤯</p>
           </div>
         </div>
-        <div className="card lg:h-[500px] max-w-[600px] flex flex-col justify-between bg-base-100 shadow-xl">
-          <div className="lg:h-[300px] mt-10 w-full overflow-hidden flex justify-center items-center">
+
+        <div className="card lg:h-[500px] max-w-[400px] flex flex-col justify-between bg-base-100 shadow-xl">
+          <div className="lg:h-[300px]  w-full overflow-hidden flex justify-center items-center">
             <figure className="px-10 pt-10 flex-shrink-0">
               <Image src="/customize-profile.jpg" alt="Notifications" className="rounded-xl" width={500} height={500} />
             </figure>
           </div>
 
           <div className="card-body items-center text-center flex-grow overflow-hidden">
-            <h2 className="card-title text-3xl font-mono">Customize your profile!</h2>
-            <p>Change colors, cover photo, create post collections, and so much more...</p>
+            <h2 className="card-title text-2xl font-mono">Display your taste! 🎨</h2>
+            <p>Create post collections, change cover photo, and so much more...</p>
           </div>
         </div>
 
-        <div className="card lg:h-[500px] max-w-[600px] flex flex-col justify-between bg-base-100 shadow-xl">
-          <div className="lg:h-[300px] mt-10 w-full overflow-hidden flex justify-center items-center">
+        <div className="card lg:h-[500px] max-w-[400px] flex flex-col justify-between items-center bg-base-100 shadow-xl">
+          <div className="lg:h-[300px]  w-full overflow-hidden flex justify-center items-center">
+            <figure className="px-10 pt-10 flex-shrink-0">
+              <Image src="/revenue.jpg" alt="Revenue" className="object-cover" width={500} height={500} />
+            </figure>
+          </div>
+          <div className="card-body items-center text-center flex-grow overflow-hidden">
+            <h2 className="card-title text-2xl font-mono">Track your revenue! 💹</h2>
+            <p>Analyze your earnings and spendings in your revenue dashboard.</p>
+          </div>
+        </div>
+
+        {/* <div className="card lg:h-[500px] max-w-[400px] flex flex-col justify-between bg-base-100 shadow-xl">
+          <figure className="px-10 pt-10 flex-shrink-0">
+            <Image src="/social-login.jpeg" alt="Notifications" className="" width={500} height={500} />
+          </figure>
+          <div className="card-body items-center text-center flex-grow overflow-hidden">
+            <h2 className="card-title text-2xl font-mono">Social login!</h2>
+            <p>Access with your wallet or your social accounts!</p>
+          </div>
+        </div> */}
+
+        <div className="card lg:h-[500px] max-w-[400px] flex flex-col justify-between bg-base-100 shadow-xl">
+          <div className="lg:h-[300px] w-full overflow-hidden flex justify-center items-center">
             <figure className="px-10 pt-10 flex-shrink-0">
               <Image src="/messaging.jpg" alt="Messaging" width={500} height={500} />
             </figure>
           </div>
           <div className="card-body items-center text-center flex-grow overflow-hidden">
-            <h2 className="card-title text-3xl font-mono">Message with others!</h2>
+            <h2 className="card-title text-2xl font-mono">Message with others! 💬</h2>
             <p>Encrypted direct messaging and group chats.</p>
           </div>
         </div>
 
-        <div className="card lg:h-[500px] max-w-[600px] flex flex-col justify-between bg-base-100 shadow-xl">
+        <div className="card lg:h-[500px] max-w-[400px] flex flex-col justify-between bg-base-100 shadow-xl">
           <figure className="px-10 pt-10 flex-shrink-0">
             <Image src="/notifications.png" alt="Notifications" className="" width={500} height={500} />
           </figure>
           <div className="card-body items-center text-center flex-grow overflow-hidden">
-            <h2 className="card-title text-3xl font-mono">Notifications!</h2>
+            <h2 className="card-title text-2xl font-mono">Notifications! 🔔</h2>
             <p>Get them on our web, on your phone, on your mail...</p>
           </div>
         </div>
 
-        <div className="card lg:h-[500px] max-w-[600px] flex flex-col justify-between bg-base-100 shadow-xl">
+        <div className="card lg:h-[500px] max-w-[400px] flex flex-col justify-between bg-base-100 shadow-xl">
           <figure className="px-10 pt-10 flex-shrink-0">
             <Image src="/accesibility.jpg" alt="Accesibility" className="" width={500} height={500} />
           </figure>
           <div className="card-body items-center text-center flex-grow overflow-hidden">
-            <h2 className="card-title text-3xl font-mono">Accesibility!</h2>
+            <h2 className="card-title text-2xl font-mono">Accesibility! 👨‍🦽</h2>
             <p>PunkSociety can be used by people with visual or hearing impairments.</p>
           </div>
         </div>
       </div>
 
-      <div className="w-full pt-2 bg-base-200">
+      <div className="w-full pt-6 bg-base-200">
         <h1 className="text-6xl font-mono text-center">Meet the team! 🤘</h1>
       </div>
       <div className="hero bg-base-200 ">
         <div className="hero-content flex-col lg:flex-row-reverse ">
-          <a
-            className="flex justify-center items-center gap-1"
-            href="https://linktr.ee/lulox"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="flex flex-col items-center gap-3 bg-base-100">
-              <figure className="px-10 pt-5 flex-shrink-0">
-                <Image src="/lulox.jpg" alt="Lulox" className="rounded-xl" width={300} height={300} />
-              </figure>
-              <div className="flex pb-5 flex-col items-center justify-center">
-                <h2 className="text-3xl">Lulox</h2>
-                <span className="italic">founder, builder</span>
-              </div>
+          <div className="flex flex-col items-center gap-3 bg-base-100 rounded-lg">
+            <figure className="px-10 pt-5 flex-shrink-0">
+              <Image src="/lulox.jpg" alt="Lulox" className="rounded-xl" width={300} height={300} />
+            </figure>
+            <div className="flex pb-5 flex-col items-center justify-center">
+              <h2 className="text-3xl">Lulox</h2>
+              <span className="italic">founder, builder</span>
+              <a
+                className="flex justify-center items-center gap-1"
+                href="https://linktr.ee/lulox"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button className="btn btn-primary mt-2">Contact me</button>
+              </a>
             </div>
-          </a>
+          </div>
         </div>
       </div>
     </div>
